0ff5b2bb89acd846b6d27478361839e6b633d653
DINA18102002 <theenaxz@gmail.com>
2026-01-22 15:17:34 +0000

    fix test page

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,11 @@ from src.main import app
 def test_index_page():
     client = app.test_client()
     response = client.get("/")
+
     assert response.status_code == 200
-    assert b"Welcome to GCP DevOps" in response.data
+
+    # Check page title / heading content
+    assert b"GCP DevOps Platform" in response.data
+    assert b"Cloud Build" in response.data
+    assert b"Artifact Registry" in response.data
+    assert b"GKE" in response.data
